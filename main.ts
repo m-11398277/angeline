4c8813655be803dfbfb9c7bae14fae7b83e50f8c
m-11398277 <116497710+m-11398277@users.noreply.github.com>
2022-10-27 09:54:49 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 basic.forever(function () {
-    if (true) {
-    	
+    if (input.lightLevel() < 80) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
     }
 })
